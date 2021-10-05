8d5cbdc8675f2af848607c14f26e017a1e0612e6
ever-dev <everdev0923@gmail.com>
2021-10-04 18:38:26 -0700

    Fix favicon

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -125,6 +125,11 @@ export const Layout: FunctionComponent = ({ children }) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/images/favicon.png"
+        />
 
         <title>CSE QuickQuote</title>
       </Head>
